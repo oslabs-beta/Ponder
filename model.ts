f8274c4e8bd17e8b6c6f421036430204edbf482f
Sbrown2018 <sara.brown07.sb@gmail.com>
2022-12-07 12:31:39 -0800

    started creating model file

---
+++ model.ts
@@ -0,0 +1,17 @@
+import { pool } from './deps.ts';
+//we create our models to either create or parse
+class Model {
+  constructor(arg){
+    this.arg;
+  }
+  //create method
+  createTable(tableName: string, args: object){
+    //convert args into SQL command to create a new table
+
+    //create a pool connection, disconnect after we create a string
+  } 
+  //parse method
+}
+
+//export this down here
+
