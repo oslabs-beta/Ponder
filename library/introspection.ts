9721fca726b7b6d5cc62ffcbfd98e0904dd3a851
Sbrown2018 <sara.brown07.sb@gmail.com>
2022-12-16 12:24:52 -0800

    We separated the introspected object over mutlitple lines, so it is easier to read

--- library/introspection.ts
+++ library/introspection.ts
@@ -6,51 +6,105 @@ import { query, poolConnection } from './connection.ts'
 //information schema provides access to database metadata
 
 
-export async function introspect () {
-  const ponder = await poolConnection(
-    "postgres://hfwbmzny:AArrmznb9EBr4Tjbxe5XordjASLQ_j4S@heffalump.db.elephantsql.com/hfwbmzny",
-    3,
-    true,
-  );
-//declare object to represent table
-  const tableObj = {}
-//query db for tables
-  const tables = await query("SELECT table_name FROM information_schema.tables WHERE table_schema='public'");//this should be an array
+export class Introspect {
+  // constructor(){
 
-  console.log('tables variable', tables);
-
-//query db for columns. query for all columns from that table
-  const columnList = await query(`SELECT column_name, is_nullable, table_name, data_type FROM information_schema.columns WHERE table_schema = 'public' ORDER BY table_name;`);
-  console.log('column list', columnList)
-// iterate through the column list
-  // tableObj[table_name][columnList[0][column_name]] = "test";
-
-  for (let i = 0; i < columnList.length; i++){
-    let colName = columnList[i]['column_name'];
-    let dataType = columnList[i]['data_type'];
-    let is_nullable = columnList[i]['is_nullable'];
-    let tableName = columnList[i]['table_name']
-    console.log('colName:', colName);
-
-    if(!tableObj[tableName]){
-        tableObj[tableName] = {}
-    }
-    tableObj[tableName][colName] = {'data_type': dataType, 'is_nullable': is_nullable};
-
+  // }
+  async introspect () {
+  //   const ponder = await poolConnection(
+  //     "postgres://hfwbmzny:AArrmznb9EBr4Tjbxe5XordjASLQ_j4S@heffalump.db.elephantsql.com/hfwbmzny",
+  //     3,
+  //     true,
+  //   );
+  //declare object to represent table
+    const tableObj = {}
+  //query db for tables
+    const tables = await query("SELECT table_name FROM information_schema.tables WHERE table_schema='public'");//this should be an array
   
+    // console.log('tables variable', tables);
+  
+  //query db for columns. query for all columns from that table
+    const columnList = await query(`SELECT column_name, is_nullable, table_name, data_type FROM information_schema.columns WHERE table_schema = 'public' ORDER BY table_name;`);
+  // iterate through the column list
+    // tableObj[table_name][columnList[0][column_name]] = "test";
+  
+    for (let i = 0; i < columnList.length; i++){
+      // const colName = columnList[i]['column_name'];
+      // const dataType = columnList[i]['data_type'];
+      // const is_nullable = columnList[i]['is_nullable'];
+      // const tableName = columnList[i]['table_name']
+      const { column_name: colName, data_type: dataType, is_nullable, table_name: tableName }: { column_name: string; data_type: string; is_nullable: string; table_name: string} = columnList[i];
+      //console.log('colName:', colName);
+  
+      if(!tableObj[tableName]){
+          tableObj[tableName] = {}
+      }
+      tableObj[tableName][colName] = {'data_type': dataType, 'is_nullable': is_nullable};
+  
+    
+    }
+    await this.classConstructor(tableObj);
+    return tableObj;
   }
-  return tableObj;
+  
+
+
+    // write func to separate our introspected object so that it's not just on one line. Be thankful for this line of psuedocode. Corey didn't want to give you this.
+    async classConstructor (tableObject: object) {
+      //iterating through our tableObject
+      for (let table in tableObject) {
+        let classString: string = '';
+        //console.log("Tables from Ikea:", table);
+        //we use concatenation to separate our massive object onto different lines
+        classString += `export class ${table} { \n`;
+        for (let column in tableObject[table]) {
+          classString  += ` ${column} = { \n`
+          // console.log('column', column);
+          const tableLength = Object.keys(tableObject[table][column]);
+          for (let i = 0; i < tableLength.length; i++) {
+            if (i === tableObject[table][column].length){
+              classString += `    ${tableLength[i]}: ${tableObject[table][column][tableLength[i]]}} \n`
+            } else{
+              classString += `    ${tableLength[i]}: ${tableObject[table][column][tableLength[i]]}, \n`
+            }
+          }
+          //close out curly braces
+          classString += '    } \n';
+        }
+        classString += '  } \n';
+        Deno.writeTextFileSync("./testClassList1.ts", classString, {
+          append: true
+        });
+      }
+    }
+
+ 
+
+      // loop 1 iterate through keys e.g tables = object.keys(tableObj), tables.forEach()
+
+        //for each key (table names) write 'export class {tablename} { \n
+
+          // loop 2 iterate through keys of each table (column names)
+            //write key(colName): { \n
+                // loop 3 iterate through values col-Name
+                  //write key: value, \n
+                   //control flow - if on last iteration (consider looping with iterator), dont add comma, add } 
+
+          // end of loop 2, concat } \n
+      
+      // end of loop 1, concat };\n 
+  
+  
+  // const testObj = await introspect();
+  
+  // console.log(testObj);
+
 }
-
-
-const testObj = await introspect();
-
-console.log(testObj);
 
 // Deno.writeTextFileSync('./testModelWrite.js', JSON.stringify(testObj));
 /*
     column_name: "diameter",
-    is_nullable: "YES",
+    is_nullable: "YES",classConstructor
     table_name: "planets",
     data_type: "integer"
 
